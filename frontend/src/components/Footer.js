c86c136351ec6bf6e75a398d20bbfd415f7c4032
Rathika <Rathika>
2025-07-04 13:50:57 +0530

    Removed Frontend folder

--- frontend/src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-export default  function Footer(){
-    return <footer className="py-1 bg-dark">
-         <p className="text-center text-white mt-1 ">
-          Selvi Snack Corner- 2025-2026, All Rights Reserved
-         </p>
-       </footer>
-
-
-}
